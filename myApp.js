40ef73d65e2e4c698dd5d1f4a800013f4c25e950
Burhaann <burhaann321@gmail.com>
2023-11-24 15:33:05 +0530

    Implementing a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -5,30 +5,30 @@ require("dotenv").config();
 console.log("Hello world");
 
 //Serve STRING
-app.get("/string", function (req, res) {
-  res.send("Hello Express");
-});
+// app.get("/string", function (req, res) {
+//   res.send("Hello Express");
+// });
 
-//Serve HTML
-absolutePath = __dirname + "/views/index.html";
+// //Serve HTML
+// absolutePath = __dirname + "/views/index.html";
 
-app.get("/", function (req, res) {
-  res.sendFile(absolutePath);
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(absolutePath);
+// });
 
-//static Public Assets
-// app.use(express.static(__dirname + "/public"));
-// app.use("/public", express.static(__dirname + "/public"));
+// //static Public Assets
+// // app.use(express.static(__dirname + "/public"));
+// // app.use("/public", express.static(__dirname + "/public"));
 
-//JSON format
-app.get("/json", function (req, res) {
-  let data = { message: "Hello json" };
-  process.env.MESSAGE_STYLE === "uppercase"
-    ? (data.message = data.message.toUpperCase())
-    : "";
+// //JSON format
+// app.get("/json", function (req, res) {
+//   let data = { message: "Hello json" };
+//   process.env.MESSAGE_STYLE === "uppercase"
+//     ? (data.message = data.message.toUpperCase())
+//     : "";
 
-  res.json(data);
-});
+//   res.json(data);
+// });
 
 //Root-Level Request Logger Middleware
 app.use(function (req, res, next) {
